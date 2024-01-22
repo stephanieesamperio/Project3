549b43ca6011195e71f2e07b6b69c97057e07af8
maijennifer <Mai.jenniferle@gmail.com>
2024-01-21 18:01:37 -0600

    Mai Edits

---
+++ Clean_CSV.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "Stephanie_CSV" (
+    "Index" int not NULL,
+    "SeeksTreatment" varchar(10)   NOT NULL,
+    "HaveBenefits" varchar(10)   NOT NULL,
+    "CareOptions" varchar(10)   NOT NULL,
+    "WellnessPrograms" varchar(10)   NOT NULL,
+    "HelpResources" varchar(10)   NOT NULL,
+    "Anonymity" varchar(10)   NOT NULL
+);
---
+++ Mai_CSV.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "Mai_CSV" (
+    "Index" int not NULL,
+    "Age" varchar(20) ,
+    "Gender" varchar(20)   NOT NULL,
+    "FamilyHistory" varchar(10)   NOT NULL,
+    "SeeksTreatment" varchar(10)   NOT NULL,
+    "HaveBenefits" varchar(10)   NOT NULL,
+    "CareOptions" varchar(10)   NOT NULL,
+    "WellnessPrograms" varchar(10)   NOT NULL,
+    "Anonymity" varchar(10)   NOT NULL
+);
