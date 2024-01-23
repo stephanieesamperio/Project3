a9779fa4c4f4eb2455e589f24120f1daa968b315
git config --global user.name Okeyoc <143902533+OkeyOc@users.noreply.github.com>
2024-01-22 20:50:25 -0600

    Add files via upload

---
+++ Mental_healthSQL.sql
@@ -0,0 +1,20 @@
+CREATE TABLE "Project_3" (
+    "Index" int not NULL,
+	"Timestamp" date   NOT NULL,
+    "Age" varchar(20)   NOT NULL,
+    "Gender" varchar(20)   NOT NULL,
+    "Country" varchar(30)   NOT NULL,
+    "FamilyHistory" varchar(10)   NOT NULL,
+    "SeeksTreatment" varchar(10)   NOT NULL,
+    "NumofEmployees" varchar(20)   NOT NULL,
+    "HaveBenefits" varchar(10)   NOT NULL,
+    "CareOptions" varchar(10)   NOT NULL,
+    "WellnessPrograms" varchar(10)   NOT NULL,
+    "HelpResources" varchar(10)   NOT NULL,
+    "Anonymity" varchar(10)   NOT NULL,
+    "MentalHealthConsequence" varchar(10)   NOT NULL,
+    "PhysicalHealthConsequence" varchar(10)   NOT NULL,
+    "MentalHealthInterview" varchar(10)   NOT NULL,
+    "PhysicalHealthInterview" varchar(10)   NOT NULL,
+    "MentalvPhysical" varchar(10)   NOT NULL
+);
